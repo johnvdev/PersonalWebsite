c0543aa0a8190e4dbd6b26bd855d6bff52d3f7fc
johnvdev1523 <johnvdev@hotmail.com>
2022-12-21 22:05:45 -0500

    home page with footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,41 @@
 @extends('layouts.app')
-
+@section('title')
+Home
+@endsection
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Welcome</div>
+<style>
+    #container {
+        background: #eaeaea;
+        height: 180px;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
+<div class="container d-flex">
+    <div class="row pt-4">
+        <div class="col-4 col-lg-2">
+            <div class="">
+                <h3 class="text-end p-4">
+                    Code Tutorials
+                    <small class="text-muted">step by step</small>
+                </h3>
+            </div>
 
-                    <div class="card-body">
-                        Coming Soon
-                    </div>
-                </div>
+        </div>
+        <div class="col-6 col-lg-3">
+            <img src="{{ asset('Images/profile.jpg') }}" class="rounded float-end img-fluid">
+        </div>
+        <div class="col-12 col-lg-7 d-flex ">
+            <div class=" ">
+                <p class="lead">
+                    Lets Code!
+                </p>
+                <p class="p-4 pb-0">
+                    Weather you are a begginer or advanced programmer im hoping my tutorials and tips can help you become a better developer. 
+                    This site is a great way for myself to also freshen up on code and technology topics while helping others begin or further their code journey.
+                </p>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="C#, Javascript, PHP, Laravel, Tutorials, Projects">
+    <meta name="author" content="John VanGeemen">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - John VanGeemen</title>
 
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
@@ -19,13 +23,31 @@
 
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Share Tech Mono">
+    <style>
+        body {
+            font-family: 'Share Tech Mono', serif;
+            background-color: #191910;
+            color: white;
+        }
+
+        p.lead {
+            font-family: 'Share Tech Mono', serif;
+        }
+
+        p {
+            font-family: 'IBM Plex Mono', serif;
+        }
+    </style>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    John VanGeemen
+                    <abbr title="Home" class="initialism">John VanGeemen</abbr>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -40,7 +62,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
-               
+
                     </ul>
                 </div>
             </div>
@@ -49,6 +71,164 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <div class="container my-5">
+            <!-- Footer -->
+            <footer class="text-center text-lg-start text-white">
+                <!-- Grid container -->
+                <div class="container p-4 pb-0">
+                    <!-- Section: Links -->
+                    <section class="">
+                        <!--Grid row-->
+                        <div class="row">
+                            <!--Grid column-->
+                            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">FOOTER CONTENT</h5>
+
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Molestiae modi cum ipsam ad, illo possimus laborum ut
+                                    reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque?
+                                    Reiciendis assumenda iusto sapiente inventore animi?
+                                </p>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">Links</h5>
+
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#!" class="text-white">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 4</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">Links</h5>
+
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#!" class="text-white">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 4</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">Links</h5>
+
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#!" class="text-white">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 4</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase">Links</h5>
+
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#!" class="text-white">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-white">Link 4</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--Grid column-->
+                        </div>
+                        <!--Grid row-->
+                    </section>
+                    <!-- Section: Links -->
+
+                    <hr class="mb-4" />
+
+                    <!-- Section: CTA -->
+                    <section class="">
+                        <p class="d-flex justify-content-center align-items-center">
+                            <span class="me-3">Register for free</span>
+                            <button type="button" class="btn btn-outline-light btn-rounded">
+                                Sign up!
+                            </button>
+                        </p>
+                    </section>
+                    <!-- Section: CTA -->
+
+                    <hr class="mb-4" />
+
+                    <!-- Section: Social media -->
+                    <section class="mb-4 text-center">
+                        <!-- Facebook -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                        <!-- Twitter -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                        <!-- Google -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                        <!-- Instagram -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                        <!-- Linkedin -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                        <!-- Github -->
+                        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+                    </section>
+                    <!-- Section: Social media -->
+                </div>
+                <!-- Grid container -->
+
+                <!-- Copyright -->
+                <div class="text-center p-3">
+                    © 2020 Copyright:
+                    <a class="text-white" href="https://johnvangeemen.ca">johnvangeemen.ca</a>
+                </div>
+                <!-- Copyright -->
+            </footer>
+            <!-- Footer -->
+        </div>
     </div>
 </body>
 </html>
